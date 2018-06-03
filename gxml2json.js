fd8f5538ed9aa9c59d2e4c71ac0dd2f963662020
ChrisKao <chi1010860@gmail.com>
2018-06-04 05:23:07 +0800

    JSON就是行!

--- gxml2json.js
+++ gxml2json.js
@@ -1,24 +1,28 @@
 const fs = require('fs')
+var keyText = []
 
 fs.readFile('static/key-text.gxml', function (err, data) {
     if (err) throw err
-    gxml = data.toString()
-    arNodeList = getNode(gxml)
-    getAttributes(arNodeList)
+    var gxml = data.toString()
+    var json = gxml2json(gxml)
+    fs.writeFile('static/key-text.json', json, function (err) {
+        if (err) throw err
+        console.log('File is saved!')
+    })
 })
 
-function getNode(_gxml) {
-    var re = /<[0-9]+ [[a-zA-Z0-9]+=".*"]*>/g
-    return _gxml.match(re)
-    // console.log(arNodeList)
+function gxml2json(_gxml) {
+    getProps(_gxml)
+    return JSON.stringify(keyText)
 }
 
-function getAttributes(_arNodeList) {
-    var re = /[[a-zA-Z0-9]+=]*/g
-    var arAttributeList = []
-    for (let i = 0; i < _arNodeList.length; i++) {
-        arAttributeList[i] = _arNodeList[i].match(re)
+function getProps(_gxml) {
+    var list = _gxml.match(/<[0-9]+ [[a-zA-Z0-9]+=".*"]*><\/>/g)
+    for (let i = 0; i < list.length; i++) {
+        var _original = list[i].match(/original=".*?"/g)[0]
+        keyText[i] = {
+            id: list[i].match(/[0-9]+/g)[0],
+            original: _original.slice(10, _original.lastIndexOf('"'))
+        }
     }
-
-    console.log(arAttributeList)
 }
--- gxml2xml.js
+++ gxml2xml.js
@@ -2,13 +2,54 @@ const fs = require('fs')
 
 fs.readFile('static/key-text.gxml', function (err, data) {
     if (err) throw err
-    gxml = data.toString()
-    arNodeList = getNode(gxml)
-    getAttributes(arNodeList)
+    var gxml = data.toString()
+    var xml = gxml2xml(gxml)
+    console.log(xml)
+    fs.writeFile('static/key-text.xml', xml, function (err) {
+        if (err) throw err
+        console.log('File is saved!')
+    })
 })
 
-function getNode(_gxml) {
-    var re = /<[0-9]+ [[a-zA-Z0-9]+=".*"]*>/g
+function gxml2xml(_gxml) {
+    var keyText = modifyParentTag(_gxml)
+    var arNodeList = getNodeList(_gxml)
+    var newNodeList = modifyNodeTag(arNodeList)
+    var _xml = remix(newNodeList)
+    return _xml
+}
+
+function modifyParentTag(str) {
+    var re = /<key-text>[\s\S]*<\/><\/>/g
+    var _keyText = str.match(re)[0].replace('</></>', '</></key-text>')
+    return _keyText
+}
+
+function getNodeList(_gxml) {
+    var re = /<[0-9]+ [[a-zA-Z0-9]+=".*"]*><\/>/g
     return _gxml.match(re)
-    // console.log(arNodeList)
+}
+
+function modifyNodeTag(_arNodeList) {
+    var tempNodeList = []
+    for (let i = 0; i < _arNodeList.length; i++) {
+        tempNodeList[i] = _arNodeList[i].replace('<', '<k')
+    }
+    var temp = []
+    var _newNodeList = []
+    for (let i = 0; i < tempNodeList.length; i++) {
+        temp[i] = tempNodeList[i].match(/<k[0-9]+/)[0]
+        var newTag = '</' + temp[i].slice(1) + '>'
+        _newNodeList[i] = tempNodeList[i].replace('</>', newTag)
+    }
+    return _newNodeList
+}
+
+function remix(_newNodeList) {
+    var temp = '<key-text>'
+    for (let i = 0; i < _newNodeList.length; i++) {
+        temp = temp.concat(_newNodeList[i])
+    }
+    temp = temp.concat('</key-text>')
+    return temp
 }
