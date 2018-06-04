a65b1d0d3fe3ffaabc3835060da8718ebe770370
ChrisKao <chi1010860@gmail.com>
2018-06-04 18:22:55 +0800

    可以從ControlLink.gxml中抓key-text

--- gxml2json.js
+++ gxml2json.js
@@ -1,28 +1,31 @@
 const fs = require('fs')
-var keyText = []
+const filePath = 'static/ControlLink.gxml'
+var gpx = {}
 
-fs.readFile('static/key-text.gxml', function (err, data) {
+// main
+fs.readFile(filePath, function (err, data) {
     if (err) throw err
-    var gxml = data.toString()
-    var json = gxml2json(gxml)
+    var gxml = data.toString()  // get the raw data
+
+    // get the data object
+    gpx.keyText = getKeyText(gxml)
 })
 
-function gxml2json(_gxml) {
-    getProps(_gxml)
-    return JSON.stringify(keyText)
-}
+// module.exports
+module.exports = gpx;
 
-function getProps(_gxml) {
-    var list = _gxml.match(/<[0-9]+ [[a-zA-Z0-9]+=".*"]*><\/>/g)
+function getKeyText(_gxml) {
+    var _keyText = []
+    var tagetTag = _gxml.match(/<key-text>[\s\S]*?(<\/>){2}/)[0]
+    var list = tagetTag.match(/<[0-9]+ [[a-zA-Z0-9]+=".*"]*><\/>/g)
     for (let i = 0; i < list.length; i++) {
         var _original = list[i].match(/original=".*?"/g)[0]
-        var _zhTw = list[i].match(/zh-tw=".*?"/g)[0]
-        keyText[i] = {
+        var _zhTw = list[i].match(/zh-tw=".*?"/g) ? list[i].match(/zh-tw=".*?"/g)[0] : list[i].match(/original=".*?"/g)[0]
+        _keyText[i] = {
             id: list[i].match(/[0-9]+/g)[0],
             original: _original.slice(10, _original.lastIndexOf('"')),
             zhTw: _zhTw.slice(7, _zhTw.lastIndexOf('"'))
         }
     }
+    return _keyText
 }
-
-module.exports = keyText;
